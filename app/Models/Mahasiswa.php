b658dffc87985dbb2e9477a80b2184b7fd0c20ff
Hanif Naufal <rafandinaufal03@gmail.com>
2023-05-30 15:47:32 +0700

    "Update Restfull API 2"

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,23 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
     use HasFactory;
+    protected $table="mahasiswa";
 
-    protected $table = 'mahasiswas'; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswas
-    public $timestamp = false;
-    protected $primaryKey = 'nim'; // Memanggil isi DB dengan Primary Key
-    public $incrementing = false;
+    public $timestamps= false;
+    protected $primaryKey = 'nim';
 
     protected $fillable = [
         'nim',
         'nama',
-        'kelas_id',
+        'kelas',
         'jurusan',
-        'no_hp',
+        'no_handphone',
+        'email',
+        'tanggal_lahir',
     ];
 }
